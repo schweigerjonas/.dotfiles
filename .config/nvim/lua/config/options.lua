ef5e67ca4692d934a34e749e74e95f281e4f2f04
Jonas Schweiger <jonas.schweiger@outlook.de>
2026-02-09 09:57:32 +0100

    configure nvim options

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,10 +2,17 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- Minimal number of screen lines to keep above and below the cursor
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+
 vim.opt.scrolloff = 10
-
--- Maximum number of characters before linebreak
 vim.opt.textwidth = 100
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.showcmd = true
+vim.opt.mouse = ""
+vim.opt.number = true
+vim.opt.ignorecase = true -- case insensitve pattern searching; can be overruled using \C
 
+vim.g.lazyvim_prettier_needs_config = true
 vim.g.lazyvim_picker = "telescope"
