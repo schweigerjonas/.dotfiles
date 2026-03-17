48b07cc3e22afbb2beb3f6e01bed0efc48546920
Jonas Schweiger <jonas.schweiger@outlook.de>
2026-03-17 12:10:42 +0100

    feat(nvim,zathura): enable inverse search from zathura instance

--- .config/nvim/lua/plugins/vimtex.lua
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -1,12 +1,12 @@
 return {
   {
     "lervag/vimtex",
-    ft = { "tex", "bib" },
+    lazy = false,
     init = function()
       vim.g.vimtex_imaps_enabled = 0 -- i.e., disable global vimtex settings
 
       vim.g.vimtex_view_method = "zathura"
-      vim.g.vimtex_view_general_options = "-reuse-instance -forward-search @tex @line @pdf"
+      -- vim.g.vimtex_view_general_options = "-reuse-instance -forward-search @tex @line @pdf"
 
       vim.g.vimtex_quickfix_open_on_warning = 0 --  don't open quickfix if there are only warnings
       vim.g.vimtex_quickfix_ignore_filters = {
