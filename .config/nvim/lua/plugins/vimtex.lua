94a7b98e0a49e98e92fd2f41a937cc04e05df385
Jonas Schweiger <jonas.schweiger@outlook.de>
2026-03-17 12:12:11 +0100

    chore(nvim): remove comment

--- .config/nvim/lua/plugins/vimtex.lua
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -6,7 +6,6 @@ return {
       vim.g.vimtex_imaps_enabled = 0 -- i.e., disable global vimtex settings
 
       vim.g.vimtex_view_method = "zathura"
-      -- vim.g.vimtex_view_general_options = "-reuse-instance -forward-search @tex @line @pdf"
 
       vim.g.vimtex_quickfix_open_on_warning = 0 --  don't open quickfix if there are only warnings
       vim.g.vimtex_quickfix_ignore_filters = {
