5af0539e45120af5570505277cee1b383ccfeb7a
Jonas Schweiger <jonas.schweiger@outlook.de>
2026-04-22 08:24:07 +0200

    feat(nvim): setup refocus after forward search with vimtex

--- .config/nvim/lua/plugins/vimtex.lua
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -2,6 +2,29 @@ return {
   {
     "lervag/vimtex",
     lazy = false,
+    config = function()
+      -- store window ID when neovim starts
+      if vim.g.vim_window_id == nil then
+        vim.g.vim_window_id = vim.fn.system("xdotool getactivewindow"):gsub("%s+", "")
+      end
+
+      local function tex_refocus_vim()
+        vim.cmd("sleep 200m")
+
+        if vim.g.vim_window_id ~= "" then
+          vim.fn.system("xdotool windowfocus " .. vim.g.vim_window_id)
+          vim.cmd("redraw!")
+        end
+      end
+
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "VimtexEventView",
+        callback = function()
+          tex_refocus_vim()
+        end,
+        desc = "Refocus Neovim after Zathura opens compiled document",
+      })
+    end,
     init = function()
       vim.g.vimtex_imaps_enabled = 0 -- i.e., disable global vimtex settings
 
