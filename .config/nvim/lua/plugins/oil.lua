730debff08aabf888063b48dacf7599fd34f69d6
Jonas Schweiger <jonas.schweiger@outlook.de>
2026-06-20 11:34:39 +0200

    feat(nvim): show hidden files in oil by default

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -6,7 +6,13 @@ return {
     opts = {
       default_file_explorer = true,
 
+      -- Skip the confirmation popup for simple operations (:help oil.skip_confirm_for_simple_edits)
       skip_confirm_for_simple_edits = true,
+
+      view_options = {
+        -- Show files and directories that start with "."
+        show_hidden = true,
+      },
     },
     dependencies = { { "nvim-mini/mini.icons", opts = {} } },
     lazy = false,
